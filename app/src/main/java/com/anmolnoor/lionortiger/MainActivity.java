ac36b99aa607ea5fe14b84ee965b6acd211538ea
Anmol Noor <anmolnoor59@gmail.com>
2020-04-08 15:58:08 +0530

    Initial commit with just simple drawables and with xml code of main layout of game or you can say app layout

---
+++ app/src/main/java/com/anmolnoor/lionortiger/MainActivity.java
@@ -0,0 +1,17 @@
+package com.anmolnoor.lionortiger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}

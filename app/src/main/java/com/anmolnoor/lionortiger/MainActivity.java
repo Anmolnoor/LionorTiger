1dbe59d844c9a713387b1a3aeaa4242b2420e1fd
Anmol Noor <anmolnoor59@gmail.com>
2020-04-08 20:06:39 +0530

    This is now with the Player code with a bug + some minor buggs fixssss

--- app/src/main/java/com/anmolnoor/lionortiger/MainActivity.java
+++ app/src/main/java/com/anmolnoor/lionortiger/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     Player[] playerChoices = new Player[9];
 
-    int [][] winnerRowColumn = {{0,1,2},{3,4,5},{6,7,8},{0,4,8},{0,3,6},{1,4,7},{2,5,8},{0,2,6}};
+    int [][] winnerRowColumn = {{0,1,2},{3,4,5},{6,7,8},{0,4,8},{0,3,6},{1,4,7},{2,5,8},{2,4,6}};
 
     TextView textView2;
 

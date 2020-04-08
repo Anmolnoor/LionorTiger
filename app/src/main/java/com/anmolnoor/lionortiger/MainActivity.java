f1d0a39f88e9ca80b971312b7db12845f719d753
Anmol Noor <anmolnoor59@gmail.com>
2020-04-08 16:27:42 +0530

    This is now with the image rotation effect to MainActivity

--- app/src/main/java/com/anmolnoor/lionortiger/MainActivity.java
+++ app/src/main/java/com/anmolnoor/lionortiger/MainActivity.java
@@ -12,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
+    public void imageIsClicked(View imageView){
+
+        ImageView tappedImageView =(ImageView) imageView;
+        tappedImageView.setTranslationX(-2000);
+        tappedImageView.setImageResource(R.drawable.tiger);
+        tappedImageView.animate().translationXBy(2000).alpha(1f).rotation(3600).setDuration(2000);
+    }
+
+
+
 }
